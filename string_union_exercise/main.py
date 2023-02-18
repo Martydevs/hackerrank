5e69a1bd0a3d895d6290b01442db0fa670b144b7
Martydevs <tmdev64@gmail.com>
2023-02-17 23:52:40 -0600

    added(directory) string union exercise

---
+++ string_union_exercise/main.py
@@ -0,0 +1,9 @@
+
+# Mostrar una lista de numeros enteros del 1 hasta n, juntarlos sin metodos de cadenas
+
+i = 0
+n = 5
+
+while i < n:
+    str(print(i + 1, end = '')) # end = '' para que no salte de linea
+    i += 1
