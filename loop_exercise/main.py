6e13ff8aa2b465361da307d17dff65141fe0ba5c
Martydevs <tmdev64@gmail.com>
2023-02-17 23:51:45 -0600

    added(directory) loop exercise

---
+++ loop_exercise/main.py
@@ -0,0 +1,14 @@
+
+# Ejercicio Ciclo
+i = 0
+
+n = int(input())
+
+# Ciclo while que itera de 0 a la entrada
+while i < n:
+
+    # numeros que sean menor a la entrada
+    # se exponenciará al cuadrado
+    if i < n:
+        print(i ** 2)
+    i += 1
