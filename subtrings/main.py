3cd764577e211c2008074fb3138900f9377da1ee
Martydevs <tmdev64@gmail.com>
2023-02-20 15:19:45 -0600

    added(exercise)

---
+++ subtrings/main.py
@@ -0,0 +1,11 @@
+
+""" def count_substring(s, ss):
+    if ss in s:
+        count = s.count(ss)
+        return count
+    else:
+        raise ValueError("substring not found") """
+
+s = input().strip()
+ss = input().strip()
+print(s, ss)
